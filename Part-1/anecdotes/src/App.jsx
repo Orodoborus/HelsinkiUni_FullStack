1f83ffcc72fe4d5cc2977669faa8606a6b853bfe
D'Andre Highfield <d.high@hotmail.com>
2024-04-22 18:41:19 -0600

    Part 1 Exercises completed 1.1 to 1.14

--- Part-1/anecdotes/src/App.jsx
+++ Part-1/anecdotes/src/App.jsx
@@ -6,6 +6,14 @@ const Button = ({ handleRandom, text }) => {
   )
 }
 
+const Title = ({ title }) => {
+  return(
+    <h1>{title}</h1>
+  )
+}
+
+let votesAry = new Uint8Array(8);
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -17,28 +25,48 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ];
-  const votesAry = new Uint8Array(8);
 
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(0);
+  const [votedAnecdote, setVotedAnecdote] = useState(0);
+  const [mostVotes, setMostVotes] = useState(0);
 
+  //handles votes
   const handleVote = () => {
-    votesAry[selected] = votesAry[selected]+1;
-    console.log(votesAry);
-    setVotes(votesAry[selected]);
+    const newVotes = votes + 1;
+    const newMostVotes = votedAnecdote;
+    setVotes(newVotes);
+    votesAry[selected] = newVotes;
+    
+    const updatedVotes = [...votesAry];
+    const favoriteAnecdote = Math.max(...updatedVotes);
+    const mostVotedAnec = updatedVotes.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+    console.log(favoriteAnecdote);
+    console.log(newVotes);
+    
+    //most voted anecdote
+    setMostVotes(favoriteAnecdote);
+    setVotedAnecdote(mostVotedAnec);
   }
 
+  //handles randomization
   const handleRandom = () =>{
     let x = Math.floor(Math.random() * 8);
     setSelected(x);
+    setVotes(votesAry[x]);
   }
-   
+
   return (
     <div>
+      <Title title='Anecdote of the day' />
       {anecdotes[selected]}
       <p>has {votes} votes</p>
       <Button handleRandom={handleVote} text='Vote' />
       <Button handleRandom={handleRandom} text='Next anecdote' />
+
+      <Title title='Anecdote with most votes' />
+      {anecdotes[votedAnecdote]}
+      <p>has {mostVotes} votes</p>
     </div>
   )
 }
